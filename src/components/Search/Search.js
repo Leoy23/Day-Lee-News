0afca7ff2a5e38d9e01ce24223b2d268c761008f
Leah Young <leah.young21@alumni.colostate.edu>
2023-01-04 18:13:56 -0700

    create search and singleArticle pages

---
+++ src/components/Search/Search.js
@@ -0,0 +1,21 @@
+// import React, { useState } from 'react';
+// import "./Search.css";
+
+
+// const Search = ({ searchArticles }) => {
+//   return (
+//     <section className='form-container'>
+//       <form>
+//         <input 
+//         type="text"
+//         placeholder="search"
+//         value={query}
+//         onChange={(e) => handleChange(e)}
+//         />
+//         <button className='search-btn' onClick={searchArticles(query)}>search</button>
+//       </form>
+//     </section>
+//   )
+// }
+
+// export default Search
